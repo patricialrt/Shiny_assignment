927b11dc7844ad16c819112af387d481775d1f76
patricialrt <patricia.bohl@gmail.com>
2016-01-31 14:42:18 +0100

    server.R and ui.R
    for my first shiny application

---
+++ ui.R
@@ -0,0 +1,18 @@
+shinyUI(pageWithSidebar(
+  headerPanel('Guerry k-means clustering'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(Guerry)),
+    selectInput('ycol', 'Y Variable', names(Guerry),
+                selected=names(Guerry)[[5]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9),
+    helpText("For a description of the Guerry data set please",
+             "type into your R Console ?Guerry."),
+    helpText("For more information on k-means, here's some first reading:",
+             "https://en.wikipedia.org/wiki/K-means_clustering")
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
+)
