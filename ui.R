1279a5dafb1b076f29f2b89b0aad35a2e1c318d4
patricialrt <patricia.bohl@gmail.com>
2016-01-31 15:25:52 +0100

    problems in shiny.io with Guerry dataset
    hope now it works

--- ui.R
+++ ui.R
@@ -1,11 +1,12 @@
 library(shiny)
 library(HistData)
+Guerry1 <- Guerry
 
 shinyUI(pageWithSidebar(
   headerPanel('Guerry k-means clustering'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(Guerry)),
-    selectInput('ycol', 'Y Variable', names(Guerry),
+    selectInput('xcol', 'X Variable', names(Guerry1)),
+    selectInput('ycol', 'Y Variable', names(Guerry1),
                 selected=names(Guerry)[[5]]),
     numericInput('clusters', 'Cluster count', 3,
                  min = 1, max = 9),
