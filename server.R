ac6b52a66dff1f44730166afc703db8ebb24a058
patricialrt <patricia.bohl@gmail.com>
2016-02-01 07:15:01 +0100

    last trial

--- server.R
+++ server.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(HistData)
-data(Guerry)
+data(Guerry, data(Guerry, local=TRUE))
 
 palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
           "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
