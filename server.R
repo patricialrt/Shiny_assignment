f17b8e4b466c37317aa2b49653595470bef8ede4
patricialrt <patricia.bohl@gmail.com>
2016-02-07 10:08:52 +0100

    next try

--- server.R
+++ server.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(HistData)
-data("Galton")
+data("Guerry")
 
 palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
           "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
@@ -10,7 +10,7 @@ shinyServer(function(input, output, session) {
   
   # Combine the selected variables into a new data frame
   selectedData <- reactive({
-    Galton[, c(input$xcol, input$ycol)]
+    Guerry[, c(input$xcol, input$ycol)]
   })
   
   clusters <- reactive({
--- ui.R
+++ ui.R
@@ -1,17 +1,20 @@
 library(shiny)
 library(HistData)
-data("Galton")
+data("Guerry")
 
 shinyUI(pageWithSidebar(
-  headerPanel('Galton k-means clustering'),
+  headerPanel('k-means clustering'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(Galton)),
-    selectInput('ycol', 'Y Variable', names(Galton),
-                selected=names(Galton)[[2]]),
+    selectInput('xcol', 'X Variable', names(Guerry)),
+    selectInput('ycol', 'Y Variable', names(Guerry),
+                selected=names(Guerry)[[5]]),
     numericInput('clusters', 'Cluster count', 3,
                  min = 1, max = 9),
-    helpText("For a description of the Guerry data set please",
-             "type into your R Console ?Guerry."),
+    helpText("Guerry is a dataset collecting social data on such things as crime,",
+             "literacy and suicide with the view to determining social laws and,",
+             "the relations among these variables. It comprises a collection of 'moral",
+             "variables' on the 86 departments of France around 1830.",
+             "For a description of the Guerry data set please check as well the R help."),
     helpText("k-means clustering aims to partition n observations",
              "into k clusters in which each observation belongs to",
              "the cluster with the nearest mean, serving as a prototype",
