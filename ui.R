80653ce8519e3d9e544c4dc01d5846954fc505bc
patricialrt <patricia.bohl@gmail.com>
2016-02-07 17:39:05 +0100

    next try

--- ui.R
+++ ui.R
@@ -1,20 +1,20 @@
 library(shiny)
 library(HistData)
-data("Guerry")
+data("Galton")
 
 shinyUI(pageWithSidebar(
   headerPanel('k-means clustering'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(Guerry)),
-    selectInput('ycol', 'Y Variable', names(Guerry),
-                selected=names(Guerry)[[5]]),
+    selectInput('xcol', 'X Variable', names(Galton)),
+    selectInput('ycol', 'Y Variable', names(Galton),
+                selected=names(Galton)[[2]]),
     numericInput('clusters', 'Cluster count', 3,
                  min = 1, max = 9),
-    helpText("The Guerry-datasets contains social data on such things as crime,",
+    helpText("The Galton-datasets contains social data on such things as crime,",
              "literacy and suicide with the view to determining social laws and,",
              "the relations among these variables. It comprises a collection of 'moral",
              "variables' on the 86 departments of France around 1830.",
-             "For a description of the Guerry-dataset please check as well the R help."),
+             "For a description of the Galton-dataset please check as well the R help."),
     helpText("k-means clustering aims to partition n observations",
              "into k clusters in which each observation belongs to",
              "the cluster with the nearest mean, serving as a prototype",
